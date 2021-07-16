e0b8605ccae6dfd29f3f79d7a1533384648f404c
Mauricio Duran Torres <mauriciodurant@gmail.com>
2021-07-16 05:51:08 -0500

    editar y dejar en bold para letras de avatar

---
+++ database/migrations/2021_07_15_000708_create_evidence_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEvidenceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evidences', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_type');
+            $table->string('name');
+            $table->string('extension_file');
+            $table->string('ip_client');
+            $table->text('metadata');
+            $table->string('hash_code');
+            $table->string('curt');
+            $table->string('case_number');
+            $table->date('date_case');
+            $table->text('labeling');
+            $table->text('name_expert');
+            $table->text('crime_address');
+            $table->text('description');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evidences');
+    }
+}
