fcdeb2cb77c948de447e789dc1bdba2de8b41c22
Mauricio Duran Torres <mauriciodurant@gmail.com>
2021-07-16 05:51:08 -0500

    trabajo con oracle y relacion con user id para tabla de evidencias

---
+++ database/factories/EvidenceFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Evidence;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EvidenceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Evidence::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
